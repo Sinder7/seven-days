5f1e4dcd358b89678412173c351834867765a935
Rasul <rasul_sadullayev@mail.ru>
2025-05-02 23:13:22 +0300

    обновленный main.py и первоначальная настройка логов

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     db_url: str = f"sqlite+aiosqlite:///./seven.db"
-    db_echo: bool = True
+    db_echo: bool = False
 
 
 config: Config = Config()
--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+import logging
+from typing import AsyncGenerator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.items.router import router as item_router
 
+logging.basicConfig(
+    level=logging.INFO,
+    filename="app_log.log",
+    filemode="a",
+    encoding="utf-8",
+    format="%(asctime)s %(levelname)s %(message)s",
+)
 
-app: FastAPI = FastAPI(title="seven-day-app", version="0.1.0")
-app.include_router(item_router)
 
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    logging.info("Инициализация приложения...  ")
+    yield
+    logging.info("Завершение работы приложения...")
+
+
+def create_app() -> FastAPI:
+
+    app: FastAPI = FastAPI(title="seven-day-app", version="0.1.0", lifespan=lifespan)
+
+    app.include_router(item_router)
+
+    return app
+
+
+app = create_app()
