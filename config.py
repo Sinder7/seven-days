53ab38fcfba7b886fefc28d8d2ee8c3aabbf9b0e
Rasul <rasul_sadullayev@mail.ru>
2025-05-15 22:41:06 +0300

    update config: create dir path

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    db_url: str = f"sqlite+aiosqlite:///./seven.db"
-    db_echo: bool = False
+
+    # daabase
+    DATABASE_URL: str = f"sqlite+aiosqlite:///./seven.db"
+    DATABASE_ECHO: bool = False
+
+    # path dir
+    PROJECT_ROOT: Path = Path(__file__).parent
+    TEMPLATES_DIR: Path = PROJECT_ROOT / "src" / "templates"
+    STATIC_DIR: Path = PROJECT_ROOT / "src" / "static"
 
 
 config: Config = Config()
